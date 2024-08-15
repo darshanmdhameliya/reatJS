163f8b233797e7f82a2379137bee27acc179e5e8
darshandhameliya000 <skillqodedarshan@gmail.com>
2024-08-15 07:56:44 +0530

    vite throw

---
+++ lecture1/reactwithvite/my-project/src/demo.jsx
@@ -0,0 +1,17 @@
+
+export const Box = {
+    'background-color':'blue',
+    'padding':'10px',
+    'border':'2px solid orange'
+}
+
+const Demo = () => {
+
+  return (
+    <div>
+      <h1 style={Box}>This is inline style</h1>
+    </div>
+  )
+}
+
+export default Demo
